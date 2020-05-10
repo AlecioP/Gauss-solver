3393041186910cf8015634a83c5134613e9b2e3c
AlecioP <alecioportaro@gmail.com>
2020-05-10 22:51:53 +0200

    commit script

--- commit.sh
+++ commit.sh
@@ -4,7 +4,12 @@ if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
 fi
 /Applications/Mathematica.app/Contents/MacOS/wolframscript Export.wls
-git add *.wl *.m Gauss.nb Gauss.txt Export.wls README.md
+git add *.wl 
+git add *.m 
+git add Gauss.nb 
+git add Gauss.txt 
+git add Export.wls 
+git add README.md
 git commit -m"$1"
 git push origin master
 exit
